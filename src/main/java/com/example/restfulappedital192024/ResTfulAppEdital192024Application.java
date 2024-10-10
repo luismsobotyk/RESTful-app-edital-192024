77ad404b395bb573dcd040f2e26d64bd0e35e895
Luis Sobotyk <luismsobotyk@gmail.com>
2024-10-10 10:20:02 -0300

    Initial commit with project structure

---
+++ src/main/java/com/example/restfulappedital192024/ResTfulAppEdital192024Application.java
@@ -0,0 +1,13 @@
+package com.example.restfulappedital192024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulAppEdital192024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulAppEdital192024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restfulappedital192024/ResTfulAppEdital192024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulappedital192024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulAppEdital192024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
